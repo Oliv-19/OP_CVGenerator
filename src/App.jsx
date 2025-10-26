4627081c8b806baf483160093c8c388991ed32f1
Oliv-19 <betza.bruguera@gmail.com>
2025-10-26 10:41:10 -0400

    mistake commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
+import GeneralInfo from './components/generalInfo'
 import './App.css'
 
 function App() {
-  
+  return(
+    <>
+    <GeneralInfo />
+
+    </>
+
+  )
 }
 
 export default App
