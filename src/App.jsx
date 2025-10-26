a50625c377fd6f5b53c53c8361d8ba712e89fea1
Oliv-19 <betza.bruguera@gmail.com>
2025-10-26 10:41:23 -0400

    Revert "mistake commit"
    This reverts commit 4627081c8b806baf483160093c8c388991ed32f1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import { useState } from 'react'
-import GeneralInfo from './components/generalInfo'
 import './App.css'
 
 function App() {
-  return(
-    <>
-    <GeneralInfo />
-
-    </>
-
-  )
+  
 }
 
 export default App
